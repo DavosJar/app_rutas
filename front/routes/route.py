e1f8ac6c06c5716f65f6e30e39da5bb0c34082e6
Alexis Jara <alexis.jara@unl.edu.ec>
2024-12-13 00:02:13 -0500

    Navegacion1

--- front/routes/route.py
+++ front/routes/route.py
@@ -5,7 +5,18 @@ import json
 import datetime
 
 router = Blueprint('router', __name__)
+URL_STATIC = 'http://localhost:8090/api'
+
 
 @router.route('/')
 def home():
-        return render_template('index.html')
+        num_conductores = get_num_items('conductor')
+        num_vehiculos = get_num_items('vehiculo')
+        num_pedidos = get_num_items('pedido')
+        
+        return render_template('index.html', num_conductores=num_conductores, num_vehiculos=num_vehiculos, num_pedidos=num_pedidos)  
+
+def get_num_items(endpoit):
+        r = requests.get(f"{URL_STATIC}/{endpoit}/list")
+        data = r.json()
+        return len(data['data'])
